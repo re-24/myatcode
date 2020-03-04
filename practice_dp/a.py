d384934b1cf5adef9ef7c4897c36cbeaf6120884
本間 裕士 <honma@cnc.local>
2020-03-04 14:44:36 +0900

    aaa

---
+++ practice_dp/a.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/dp/tasks/dp_a
+
+n=int(input())
+h=list(map(int, input().split()))
+
+dp=[10**5 for i in range(n)]
+dp[0]=0
+for i in range(n):
+    if i+1<n:dp[i+1] = min(dp[i+1], dp[i] + abs(h[i+1]-h[i]))
+    if i+2<n:dp[i+2] = min(dp[i+2], dp[i] + abs(h[i+2]-h[i]))
+
+print(dp[n-1])
