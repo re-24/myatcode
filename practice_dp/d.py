ea22d38a822954a53b17042d8feb8179606b4db3
本間 裕士 <honma@cnc.local>
2020-03-16 16:59:53 +0900

    dp d tle

--- practice_dp/d.py
+++ practice_dp/d.py
@@ -1,15 +1,15 @@
 N,W=map(int, input().split())
-w=[None]*10**5
-v=[None]*10**9
+w=[None]*100
+v=[None]*100
 for i in range(N):
     w[i],v[i]=map(int, input().split())
 
-dp=[[0]*N for _ in range(W+1)]
+dp=[[0]*(W+1) for _ in range(N+1)]
 
 for i in range(N):
-    for sum_w in range(W+1):
-        if sum_w >= w[i]:
-            dp[i+1][sum_w] = max(dp[i][sum_w-w[i]]+v[i], dp[i][sum_w])
+    for weight in range(W+1):
+        if weight - w[i] >= 0:
+            dp[i+1][weight] = max(dp[i][weight-w[i]]+v[i], dp[i][weight])
         else:
-            dp[i+1][sum_w] = dp[i][sum_w]
+            dp[i+1][weight] = dp[i][weight]
 print(max(dp[N]))
